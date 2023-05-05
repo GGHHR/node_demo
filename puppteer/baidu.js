84165cc1441ab7ec0cc851f734d16f12f789780d
2 <1241639333@qq.com>
2023-05-05 10:45:04 +0800

    添加electron

--- puppteer/baidu.js
+++ puppteer/baidu.js
@@ -1,10 +1,11 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://www.baidu.com');
-    const content = await page.content();
-    console.log(content);
+    await page.goto('https://www.baidu.com/', { waitUntil: 'networkidle2' });
+    const html = await page.content();
+    fs.writeFileSync('baidu.html', html);
     await browser.close();
 })();
