9f3623a62cd3f02bc9a9133b9805606f230297a5
2 <1241639333@qq.com>
2023-10-08 10:23:51 +0800

    1

--- v2rayn订阅/index.js
+++ v2rayn订阅/index.js
@@ -1,25 +1,33 @@
 const puppeteer = require('puppeteer');
 const sqlite3 = require('sqlite3').verbose();
+const ps = require('ps-node');
+const path = require('path');
 
-async function getRunningV2raynPath() {
-    try {
-        const { default: psList } = await import('ps-list');
+function getRunningV2raynPath() {
+    return new Promise((resolve, reject) => {
+        ps.lookup({
+            command: 'v2rayn.exe',
+        }, (err, resultList) => {
+            if (err) {
+                console.log('查询过程中出现错误：', err);
+                reject(err);
+                return;
+            }
 
-        const processes = await psList();
+            if (resultList.length > 0) {
+                const v2raynProcess = resultList[0];
+                const parsedPath = path.parse(v2raynProcess.command);
 
-        const v2raynProcess = processes.find(process => process.name.includes('v2rayN'));
-
-        if (v2raynProcess) {
-            console.log('v2rayn 正在运行，路径：', v2raynProcess.cmd);
-        } else {
-            console.log('v2rayn 未在运行');
-        }
-    } catch (error) {
-        console.log('查询过程中出现错误：', error.message);
-    }
+                resolve(parsedPath.dir);
+            } else {
+                console.log('v2rayn 未在运行');
+                resolve(null);
+            }
+        });
+    });
 }
 
-getRunningV2raynPath();
+
 class SubGet {
     async initialize(url, listEl, el, remarks, id) {
         this.url = url;
@@ -71,6 +79,7 @@ class SubGet {
         }
     }
 }
+
 class SubGet1 {
     async initialize(url, el, remarks, id) {
         this.url = url;
@@ -119,50 +128,61 @@ class SubGet1 {
 
 class UpSubItem {
     constructor(url, remarks, id, convertTarget) {
-        const outputValue = 'D:/APP/v2rayN-With-Core/guiConfigs/guiNDB.db'; // 替换为实际的输出路径
-        // 打开数据库连接
-        const db = new sqlite3.Database(outputValue, sqlite3.OPEN_READWRITE, (err) => {
-            if (err) {
-                console.error(err.message);
-            } else {
-                // console.log(`Connected to the ${dbFilePath} database.`);
-            }
-        });
+         getRunningV2raynPath()
+            .then(command => {
+                if (command) {
 
-        // 定义 SQL 语句以插入或替换 SubItem 表中的记录
-        const insertOrUpdateSql = `INSERT OR REPLACE INTO SubItem (remarks, url, id, convertTarget) VALUES (?, ?, ?, ?)`;
+                    const outputValue = path.join(command,'guiConfigs/guiNDB.db'); // 替换为实际的输出路径
+                    console.log(outputValue)
+                    // 打开数据库连接
+                    const db = new sqlite3.Database(outputValue, sqlite3.OPEN_READWRITE)
 
-        // 执行 SQL 语句
-        db.run(insertOrUpdateSql, [remarks, url, id, convertTarget], function(err) {
-            if (err) {
-                console.error(err.message);
-            } else {
-                // console.log(`Row(s) updated: ${this.changes}`);
-            }
+                    // 定义 SQL 语句以插入或替换 SubItem 表中的记录
+                    const insertOrUpdateSql = `INSERT OR REPLACE INTO SubItem (remarks, url, id, convertTarget) VALUES (?, ?, ?, ?)`;
 
-            // 关闭数据库连接
-            db.close((err) => {
-                if (err) {
-                    console.error(err.message);
+                    // 执行 SQL 语句
+                    db.run(insertOrUpdateSql, [remarks, url, id, convertTarget], function (err) {
+                        if (err) {
+                            console.error(err.message);
+                        } else {
+                            // console.log(`Row(s) updated: ${this.changes}`);
+                        }
+
+                        // 关闭数据库连接
+                        db.close((err) => {
+                            if (err) {
+                                console.error(err.message);
+                            } else {
+                                // console.log('Closed the database connection.');
+                            }
+                        });
+                    });
                 } else {
-                    // console.log('Closed the database connection.');
+                    console.log('v2rayn 未在运行');
                 }
+                return command;
+            })
+            .catch(err => {
+                console.log('获取路径时出现错误：', err);
             });
-        });
+
+
     }
 }
 
 async function main() {
 
+
+
     // 进来是列表的那种
-    await new SubGet().initialize("https://nodefree.org/", ".item-title a", ".section p","a1","1");
-    await new SubGet().initialize("https://clashnode.com/", "[cp-post-title] a", ".post-content-content h2+p+p+p","a2","2");
-    await new SubGet().initialize("https://v2cross.com/", ".entry-title a", ".entry-content h5","a3","3");
-    await new SubGet().initialize("https://clashgithub.com/", "[itemprop=\"name headline\"] a", ".article-content p:nth-child(11)","a4","4");
-    await new SubGet().initialize("https://www.iyio.net/", ".column article:nth-child(4) a", "pre","a5","5");
-    await new SubGet().initialize("https://kkzui.com/", ".row  .url-card:last-child a", ".panel-body p:nth-child(7)","a6","6");
+    await new SubGet().initialize("https://nodefree.org/", ".item-title a", ".section p", "a1", "1");
+    await new SubGet().initialize("https://clashnode.com/", "[cp-post-title] a", ".post-content-content h2+p+p+p", "a2", "2");
+    await new SubGet().initialize("https://v2cross.com/", ".entry-title a", ".entry-content h5", "a3", "3");
+    await new SubGet().initialize("https://clashgithub.com/", "[itemprop=\"name headline\"] a", ".article-content p:nth-child(11)", "a4", "4");
+    await new SubGet().initialize("https://www.iyio.net/", ".column article:nth-child(4) a", "pre", "a5", "5");
+    await new SubGet().initialize("https://kkzui.com/", ".row  .url-card:last-child a", ".panel-body p:nth-child(7)", "a6", "6");
     // 进来直接找链接
-    await new SubGet1().initialize("https://wanshanziwo.eu.org/", ".is-fullwidth tr:nth-child(3) td","b1","1000");
+    await new SubGet1().initialize("https://wanshanziwo.eu.org/", ".is-fullwidth tr:nth-child(3) td", "b1", "1000");
 }
 
 main();
