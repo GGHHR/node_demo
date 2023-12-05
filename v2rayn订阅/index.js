507c25291da2fd7fb943ecda3c7d4d23142ccd09
2 <1241639333@qq.com>
2023-12-05 15:15:51 +0800

    1

--- v2rayn订阅/index.js
+++ v2rayn订阅/index.js
@@ -1,9 +1,8 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 const sqlite3 = require('sqlite3').verbose();
 const ps = require('ps-node');
 const fs = require("fs");
 const path = require("path");
-
 
 function getRunningV2raynPath() {
     return new Promise((resolve, reject) => {
@@ -29,12 +28,12 @@ function getRunningV2raynPath() {
     });
 }
 
-
 class SubGet {
     constructor(browser) {
         this.browser = browser;
     }
-    async initialize(url,sel, remarks, id) {
+
+    async initialize(url, sel, remarks, id) {
         this.url = url;
         this.listEl = sel[0];
         this.el = sel[1];
@@ -49,113 +48,102 @@ class SubGet {
 
         console.log('正在：' + this.url);
 
-        await page.goto(this.url,{timeout:99999});
+        await page.goto(this.url, { timeout: 99999 });
         let content;
         if (this.listEl) {
-            await page.waitForSelector(this.listEl,{timeout:99999});
+            await page.waitForSelector(this.listEl, { timeout: 99999 });
             content = await page.$eval(this.listEl, element => element.href);
-            await page.goto(content,{timeout:99999});
+            await page.goto(content, { timeout: 99999 });
             console.log('正在：' + content);
         }
-        await page.waitForSelector(this.el,{timeout:99999});
+        await page.waitForSelector(this.el, { timeout: 99999 });
         content = await page.$eval(this.el, element => element.textContent);
 
-        // 定义匹配URL的正则表达式模式
         const urlPattern = /https?:\/\/[^\s/$.?#].[^\s]*/gi;
-
-        // 查找匹配的链接
         const match = content.match(urlPattern)[0];
 
-        // 输出匹配的链接
         let convertTarget = "";
-        if (match.endsWith("yaml"||"yml")) {
+        if (match.endsWith("yaml" || "yml")) {
             convertTarget = "mixed";
         }
         console.log(`链接${this.remarks}：${match}`);
-        // 调用 UpSubItem.Up() 函数
-        await  UpSubItem(match, this.remarks, this.id, convertTarget); // 等待函数完成
-
+        await UpSubItem(match, this.remarks, this.id, convertTarget);
 
         await page.close();
     }
 }
 
-    async function UpSubItem(url, remarks, id, convertTarget) {
-        try {
-            const command = await getRunningV2raynPath();
-            if (command) {
-                const outputValue = path.join(command, 'guiConfigs/guiNDB.db');
-                const db = new sqlite3.Database(outputValue, sqlite3.OPEN_READWRITE);
-                const insertOrUpdateSql = `INSERT OR REPLACE INTO SubItem (remarks, url, id, convertTarget,sort) VALUES (?, ?, ?, ?, ?)`;
+async function UpSubItem(url, remarks, id, convertTarget) {
+    try {
+        const command = await getRunningV2raynPath();
+        if (command) {
+            const outputValue = path.join(command, 'guiConfigs/guiNDB.db');
+            const db = new sqlite3.Database(outputValue, sqlite3.OPEN_READWRITE);
+            const insertOrUpdateSql = `INSERT OR REPLACE INTO SubItem (remarks, url, id, convertTarget,sort) VALUES (?, ?, ?, ?, ?)`;
 
-                db.run(insertOrUpdateSql, [remarks, url, id, convertTarget, id], function (err) {
+            db.run(insertOrUpdateSql, [remarks, url, id, convertTarget, id], function (err) {
+                if (err) {
+                    console.error(err.message);
+                }
+                db.close((err) => {
                     if (err) {
                         console.error(err.message);
                     }
-                    db.close((err) => {
-                        if (err) {
-                            console.error(err.message);
-                        }
-                    });
                 });
-            } else {
-                console.log('v2rayn 未在运行');
-            }
-
-            return command;
-        } catch (error) {
-            console.error('处理路径时出现错误：', error);
+            });
+        } else {
+            console.log('v2rayn 未在运行');
         }
-    }
 
+        return command;
+    } catch (error) {
+        console.error('处理路径时出现错误：', error);
+    }
+}
 
 async function main() {
-    let select ;
+    let select;
 
-    let browser =   await puppeteer.launch({
+    let browser = await puppeteer.launch({
         headless: "new",
         slowMo: 250,
-        executablePath: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe',
+        executablePath: path.resolve('C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe'),
     });
 
     try {
         console.log('请求远程json中')
         const page = await browser.newPage();
-        let  url='https://raw.githubusercontent.com/GGHHR/node_demo/master/v2rayn%E8%AE%A2%E9%98%85/init.json';
-        await page.goto(url,{timeout:99999});
-        await page.waitForSelector('pre',{timeout:99999});
-        let  content = await page.$eval('pre', element => element.textContent);
-        content=JSON.parse(content);
-        select=content;
+        let url = 'https://raw.githubusercontent.com/GGHHR/node_demo/master/v2rayn%E8%AE%A2%E9%98%85/init.json';
+        await page.goto(url, { timeout: 99999 });
+        await page.waitForSelector('pre', { timeout: 99999 });
+        let content = await page.$eval('pre', element => element.textContent);
+        content = JSON.parse(content);
+        select = content;
         await page.close();
         console.log('请求成功')
-    }catch (e){
+    } catch (e) {
 
-        select  = JSON.parse(fs.readFileSync('./init.json', 'utf8'));
+        select = JSON.parse(fs.readFileSync(path.join(__dirname, 'init.json'), 'utf8'));
         console.log('请求失败，用本地的json文件')
     }
 
-
     await Promise.all(select.select.map(async (v, i) => {
-        v.id=i+1;
+        v.id = i + 1;
         try {
             await new SubGet(browser).initialize(v.url, v.sel, i + 1, i + 1);
-            v.update=true;
+            v.update = true;
         } catch (e) {
-            v.update=false;
-            v.shibai=v.shibai?v.shibai+1:1;
-            console.log(`${i + 1}失败：`+ v.url);
+            v.update = false;
+            v.shibai = v.shibai ? v.shibai + 1 : 1;
+            console.log(`${i + 1}失败：` + v.url);
         }
     }));
 
     await cleanupDatabase(select.select.length);
-    await fs.writeFileSync('./init.json',JSON.stringify(select),'utf-8');
+    await fs.writeFileSync(path.join(__dirname, 'init.json'), JSON.stringify(select), 'utf-8');
     await browser.close()
     await process.exit(0);
-
 }
-
-main();
 
 const cleanupDatabase = async (num) => {
     try {
@@ -189,3 +177,4 @@ const cleanupDatabase = async (num) => {
     }
 };
 
+main();
